1939fb1aeb8c7050bede120fb91ea3f6bdf2a271
Johnny <johnnyq@hotmail.fr>
2024-08-04 01:22:09 +0200

    feat: update the informations logged by the startup + update readme to include the startup script

--- startup/main.sh
+++ startup/main.sh
@@ -3,6 +3,11 @@
 cat ./tamagokill.ascii
 cat ./startup.ascii
 
+problem_count=0
+incr_pbs () {
+  problem_count=$((problem_count+1))
+}
+
 cmd_exists () {
   if command -v $1 &> /dev/null
   then
@@ -30,6 +35,7 @@ step_requirements() {
     node_version=$(node -v)
     npm_version=$(npm -v)
   else
+    incr_pbs ;
     node_version=" n/a"
     npm_version=" n/a"
     error_msgs+=("Node.js & npm are mandatory to build & run the Web part." "They are is also needed to run some helpful scripts to manipulate the project as a whole.")
@@ -40,22 +46,37 @@ step_requirements() {
   then
     cargo_version=$(cargo -V)
   else
+    incr_pbs ;
     cargo_version=" n/a"
     error_msgs+=("Cargo is mandatory to build & run the API part. Rust relies on it.")
     links_msgs+=("Cargo: https://doc.rust-lang.org/cargo/getting-started/installation.html")
   fi
+
+  if cmd_exists "docker" ;
+  then
+    docker_version=$(docker -v)
+  else
+    incr_pbs ;
+    docker_version=" n/a"
+    error_msgs+=("Docker is mandatory to run the containerized environment.")
+    links_msgs+=("Docker: https://docs.docker.com/")
+  fi
+
 
   disp_title "Requirements check";
 
   echo "Node version:    " ${node_version}
   echo "Npm version:     " ${npm_version}
   echo "Cargo version:   " ${cargo_version}
+  echo "Docker version:  " ${docker_version}
 
+  echo ""
   if ! [ ${#error_msgs[@]} -eq 0 ];
   then
-    echo ""
     echo "--- Problems ---"
     printf '> %s\n' "${error_msgs[@]}"
+  else
+    echo "You got all the mandatory tools!"
   fi
 
   if ! [ ${#links_msgs[@]} -eq 0 ];
@@ -69,8 +90,22 @@ step_requirements() {
 
 step_setup_hooks() {
   disp_title "Setup hooks"
-  cp -r ./git-hooks/* ../.git/hooks/
+  if cp -r ./git-hooks/* ../.git/hooks/ ;
+  then
+    echo "Hooks installed in the repository."
+  else
+    incr_pbs ;
+    echo "Couldn't install the hooks"
+  fi
 }
 
 step_requirements ;
 step_setup_hooks ;
+
+disp_title "Finalize"
+if [ ${problem_count} -eq 0 ]
+then
+  echo "You are all set!"
+else
+  echo "You got ${problem_count} errors(s). Review the setup and try again when you solved it!"
+fi
